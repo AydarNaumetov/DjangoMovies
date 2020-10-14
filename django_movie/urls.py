b9a75d97eb8f9e927b65d7ace65b171497ae7ac0
AydarNaumetov <aydar_naumetov@mail.ru>
2020-10-14 12:36:29 +0600

    20th lesson

--- django_movie/urls.py
+++ django_movie/urls.py
@@ -1,7 +1,6 @@
 """django_movie URL Configuration
-
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -18,11 +17,10 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include("movies.urls")),
     path('ckeditor/', include('ckeditor_uploader.urls')),
+    path("", include("movies.urls")),
 ]
 
 if settings.DEBUG:
